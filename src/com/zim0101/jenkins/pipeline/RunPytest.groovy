beb47fd2883ba2732ae4dec8cad3c2b52444b203
zim0101 <cybertronix.4406@gmail.com>
2023-08-07 15:17:54 +0600

    update jenkins shared lib

--- src/com/zim0101/jenkins/pipeline/RunPytest.groovy
+++ src/com/zim0101/jenkins/pipeline/RunPytest.groovy
@@ -4,12 +4,12 @@ package com.zim0101.jenkins.pipeline
 def execute(String pythonVersion) {
     catchError () {
         ansiColor('xterm') {
-            sh '''#!/usr/bin/env bash
+            sh """#!/usr/bin/env bash
                 set -ex
                 cd ${pythonVersion}
                 pip install .
                 pytest
-            '''
+            """
         }
     }
 }
